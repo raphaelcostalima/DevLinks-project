420f5973f83cc5fe506b998c1247544d7a24dd48
Raphael Afonso <rapha_afonso@outlook.com>
2024-05-27 16:52:24 -0300

    change media query

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function toggleMode() {
   const img = document.querySelector(".profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "https://github.com/raphaelcostalima.png")
-  } else {
     img.setAttribute("src", "./assets/light-photo-avatar.jpg")
     img.setAttribute("alt", "Cap avatar profile picture dark mode")
+  } else {
+    img.setAttribute("src", "https://github.com/raphaelcostalima.png")
   }
 }
